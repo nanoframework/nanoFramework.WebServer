aa000bef91d2d43547655678afe986fc863a60f2
Laurent Ellerbach <laurelle@microsoft.com>
2024-01-26 14:19:48 +0100

    add status events

---
+++ nanoFramework.WebServer/WebServerStatus.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2020 Laurent Ellerbach and the project contributors
+// See LICENSE file in the project root for full license information.
+
+namespace nanoFramework.WebServer
+{
+    /// <summary>
+    /// Represents the status of the server.
+    /// </summary>
+    public enum WebServerStatus
+    {
+        /// <summary>
+        /// The server is stopped.
+        /// </summary>
+        Stopped,
+
+        /// <summary>
+        /// The server is running.
+        /// </summary>
+        Running,
+    }
+}
---
+++ nanoFramework.WebServer/WebServerStatusEventArgs.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2020 Laurent Ellerbach and the project contributors
+// See LICENSE file in the project root for full license information.
+
+namespace nanoFramework.WebServer
+{
+    /// <summary>
+    /// Provides data for the WebServerStatus event.
+    /// </summary>
+    public class WebServerStatusEventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the WebServerStatusEventArgs class with the specified status.
+        /// </summary>
+        /// <param name="status">The status of the web server.</param>
+        public WebServerStatusEventArgs(WebServerStatus status)
+        {
+            Status = status;
+        }
+
+        /// <summary>
+        /// Gets the status of the web server.
+        /// </summary>
+        public WebServerStatus Status { get; protected set; }
+    }
+}
