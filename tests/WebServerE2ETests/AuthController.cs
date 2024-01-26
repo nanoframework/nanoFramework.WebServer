0193d887595deb3eaf468cdf9d58ec5f888d88a7
Laurent Ellerbach <laurelle@microsoft.com>
2024-01-26 13:09:03 +0100

    Adding E2E test application and fixing OutputHttpCode when no content (#240)

---
+++ tests/WebServerE2ETests/AuthController.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2020 Laurent Ellerbach and the project contributors
+// See LICENSE file in the project root for full license information.
+
+using nanoFramework.WebServer;
+using System.Net;
+
+namespace WebServerE2ETests
+{
+    [Authentication("Basic")]
+    class AuthController
+    {
+        [Route("authbasic")]
+        public void Basic(WebServerEventArgs e)
+        {
+            WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.OK);
+        }
+
+        [Route("authbasicspecial")]
+        [Authentication("Basic:user2 password")]
+        public void Special(WebServerEventArgs e)
+        {
+            WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.OK);
+        }
+
+        [Authentication("ApiKey:superKey1234")]
+        [Route("authapi")]
+        public void Key(WebServerEventArgs e)
+        {
+            WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.OK);
+        }
+
+        [Route("authnone")]
+        [Authentication("None")]
+        public void None(WebServerEventArgs e)
+        {
+            WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.OK);
+        }
+
+        [Authentication("ApiKey")]
+        [Route("authdefaultapi")]
+        public void DefaultApi(WebServerEventArgs e)
+        {
+            WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.OK);
+        }
+    }
+}
---
+++ tests/WebServerE2ETests/Program.cs
@@ -0,0 +1,105 @@
+// Copyright (c) 2020 Laurent Ellerbach and the project contributors
+// See LICENSE file in the project root for full license information.
+
+using nanoFramework.Networking;
+using nanoFramework.WebServer;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace WebServerE2ETests
+{
+    public class Program
+    {
+        private const string Ssid = "yourSSID";
+        private const string Password = "YourPAssword";
+        private static WebServer _server;
+
+        public static void Main()
+        {
+            Debug.WriteLine("Hello from nanoFramework WebServer end to end tests!");
+
+            var res = WifiNetworkHelper.ConnectDhcp(Ssid, Password, requiresDateTime: true, token: new CancellationTokenSource(60_000).Token);
+            if (!res)
+            {
+                Debug.WriteLine("Impossible to connect to wifi, most likely invalid credentials");
+                return;
+            }
+
+            Debug.WriteLine($"Connected with wifi credentials. IP Address: {GetCurrentIPAddress()}");
+            _server = new WebServer(80, HttpProtocol.Http, new Type[] { typeof(SimpleRouteController), typeof(AuthController) });
+            // To test authentication with various scenarios
+            _server.ApiKey = "ATopSecretAPIKey1234";
+            _server.Credential = new NetworkCredential("topuser", "topPassword");
+            // Add a handler for commands that are received by the server.
+            _server.CommandReceived += ServerCommandReceived;
+
+            // Start the server.
+            _server.Start();
+
+            Thread.Sleep(Timeout.Infinite);
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+
+        private static void ServerCommandReceived(object obj, WebServerEventArgs e)
+        {
+            const string FileName = "I:\\Text.txt";
+            var url = e.Context.Request.RawUrl;
+            Debug.WriteLine($"{nameof(ServerCommandReceived)} {e.Context.Request.HttpMethod} {url}");
+
+            if (url.ToLower().IndexOf("/param.htm") == 0)
+            {
+                // Test with parameters
+                var parameters = WebServer.DecodeParam(url);
+                string toOutput = "<html><head>" +
+                    "<title>Hi from nanoFramework Server</title></head><body>Here are the parameters of this URL: <br />";
+                foreach (var par in parameters)
+                {
+                    toOutput += $"Parameter name: {par.Name}, Value: {par.Value}<br />";
+                }
+                toOutput += "</body></html>";
+                WebServer.OutPutStream(e.Context.Response, toOutput);
+                return;
+            }
+            else if (url.IndexOf("/Text.txt") == 0)
+            {
+                if (File.Exists(FileName))
+                {
+                    WebServer.SendFileOverHTTP(e.Context.Response, FileName);
+                    return;
+                }
+                else
+                {
+                    WebServer.OutputHttpCode(e.Context.Response, HttpStatusCode.NotFound);
+                    return;
+                }
+            }
+            else if (url.ToLower().IndexOf("/useinternal") == 0)
+            {
+                File.WriteAllText(FileName, "This is a test file for WebServer");
+            }
+            else
+            {
+                WebServer.OutPutStream(e.Context.Response, "<html><head>" +
+                    "<title>Hi from nanoFramework Server</title></head><body>You want me to say hello in a real HTML page!<br/><a href='/useinternal'>Generate an internal text.txt file</a><br />" +
+                    "<a href='/Text.txt'>Download the Text.txt file</a><br>" +
+                    "Try this url with parameters: <a href='/param.htm?param1=42&second=24&NAme=Ellerbach'>/param.htm?param1=42&second=24&NAme=Ellerbach</a></body></html>");
+                return;
+            }
+        }
+
+        public static string GetCurrentIPAddress()
+        {
+            NetworkInterface ni = NetworkInterface.GetAllNetworkInterfaces()[0];
+
+            // get first NI ( Wifi on ESP32 )
+            return ni.IPv4Address.ToString();
+        }
+    }
+}
