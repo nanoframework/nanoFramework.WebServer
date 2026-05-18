e4e4fa1b2c3e965ac12dd2227bf5ddfaa3f25f16
José Simões <jose.simoes@eclo.solutions>
2026-05-18 11:24:03 +0100

    Harden WebServer listener and ReadBody (#356)

--- nanoFramework.WebServer/HttpListenerRequestExtensions.cs
+++ nanoFramework.WebServer/HttpListenerRequestExtensions.cs
@@ -21,45 +21,82 @@ namespace nanoFramework.WebServer
             MultipartFormDataParser.Parse(httpListenerRequest.InputStream);
 
         /// <summary>
-        /// Reads a body from the HttpListenerRequest inputstream
+        /// Reads a body from the HttpListenerRequest inputstream.
         /// </summary>
         /// <param name="httpListenerRequest">The request to read the body from</param>
-        /// <returns>A byte[] containing the body of the request</returns>
+        /// <returns>
+        /// A byte[] containing the body of the request, or <see langword="null"/> if the body could not be read.
+        /// </returns>
         public static byte[] ReadBody(this HttpListenerRequest httpListenerRequest)
         {
-            byte[] body = new byte[httpListenerRequest.ContentLength64];
-            byte[] buffer = new byte[4096];
-            Stream stream = httpListenerRequest.InputStream;
+            long contentLength = httpListenerRequest.ContentLength64;
 
-            int position = 0;
-
-            while (true)
+            // check missing or invalid content-length
+            if (contentLength <= 0)
             {
-                // The stream is (should be) a NetworkStream which might still be receiving data while
-                // we're already processing. Give the stream a chance to receive more data or we might
-                // end up with "zero bytes read" too soon...
-                Thread.Sleep(1);
-
-                long length = stream.Length;
-
-                if (length > buffer.Length)
-                {
-                    length = buffer.Length;
-                }
-
-                int bytesRead = stream.Read(buffer, 0, (int)length);
-
-                if (bytesRead == 0)
-                {
-                    break;
-                }
-
-                Array.Copy(buffer, 0, body, position, bytesRead);
-
-                position += bytesRead;
+                return new byte[0];
             }
 
-            return body;
+            // Sanity check for huge content-length
+            // A managed array cannot exceed int.MaxValue elements
+            // Treat an oversized Content-Length the same as an allocation failure.
+            if (contentLength > int.MaxValue)
+            {
+                return null;
+            }
+
+            try
+            {
+                int bodySize = (int)contentLength;
+                byte[] body = new byte[bodySize];
+                byte[] buffer = new byte[4096];
+                Stream stream = httpListenerRequest.InputStream;
+
+                int position = 0;
+
+                while (position < bodySize)
+                {
+                    // The stream is (should be) a NetworkStream which might still be receiving data while
+                    // we're already processing. Give the stream a chance to receive more data or we might
+                    // end up with "zero bytes read" too soon...
+                    Thread.Sleep(1);
+
+                    long length = stream.Length;
+
+                    if (length <= 0)
+                    {
+                        break;
+                    }
+
+                    if (length > buffer.Length)
+                    {
+                        length = buffer.Length;
+                    }
+
+                    long remaining = bodySize - position;
+                    if (length > remaining)
+                    {
+                        length = remaining;
+                    }
+
+                    int bytesRead = stream.Read(buffer, 0, (int)length);
+
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    Array.Copy(buffer, 0, body, position, bytesRead);
+
+                    position += bytesRead;
+                }
+
+                return body;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
